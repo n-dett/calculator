01f5fd9c51de70b0f418569372254e7aec65a555
n-dett <124851780+n-dett@users.noreply.github.com>
2023-04-26 21:52:05 -0500

    Create operation functions
    Added functions for add, subtract, multiply, and divide.

---
+++ calculator.js
@@ -0,0 +1,31 @@
+let firstNum
+let operator
+let secondNum
+
+// Operation
+function operate(num1, num2, callback) {
+    return callback(num1, num2);
+}
+
+// Create functions for add, subtract, multiply, and divide
+
+// Add
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+// Subtract
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+// Multiply
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+// Divide
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
